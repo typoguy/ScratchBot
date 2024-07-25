4159da369b39b0ba082a35e1fe1813bd886e6d59
typoguy <typoguy@users.noreply.github.com>
2024-07-24 19:26:33 -0500

    Initial testing with ruff.. looks good to me!

--- scratchbot/config.py
+++ scratchbot/config.py
@@ -1,18 +1,19 @@
 import json
 from sys import exit
 
+
 class Config:
-	def __init__(self):
-		self.configfile = 'config.json'
-		self.load()
+    def __init__(self):
+        self.configfile = "config.json"
+        self.load()
 
-	def load(self):
-		with open(self.configfile, 'r') as config_file:
-			self.config = json.load(config_file)
+    def load(self):
+        with open(self.configfile, "r") as config_file:
+            self.config = json.load(config_file)
 
-	def get(self, entry):
-		try:
-			return self.config[entry]
-		except KeyError as error:
-			print('Error: {} not found in {}'.format(error, self.configfile))
-			exit()
+    def get(self, entry):
+        try:
+            return self.config[entry]
+        except KeyError as error:
+            print("Error: {} not found in {}".format(error, self.configfile))
+            exit()
